5fb04f16f4672e1d3d5512165bc3aa90bee53f18
DJ On Top <djontopmv@gmail.com>
2025-03-06 01:52:28 +0530

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'DJ ✔ 👑 [https://tinyurl.com/djontop] '
+description 'Anti-Metagaming Ensures players are in the required Discord voice channel'
+version '1.0.0'
+
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
+
+
